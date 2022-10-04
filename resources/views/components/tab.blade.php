60f4a67c3073c091c56c1a840bee1f55b2200299
aerrata <0x0976@gmail.com>
2022-10-04 22:20:22 +0800

    feat

--- resources/views/components/tab.blade.php
+++ resources/views/components/tab.blade.php
@@ -7,7 +7,7 @@
 <ul {!! $attributes->merge(['class' => 'nav nav-bordered']) !!}>
     @foreach ($links as $link)
     <li class="nav-item">
-        <a @class(['active'=> (strpos(Route::currentRouteName(), $link['active']) === 0), 'nav-link']) href="{{ $link['url'] }}">{{ $link['label'] }}</a>
+        <a @class(['active'=> (strpos(Route::currentRouteName(), @$link['active']) === 0), 'nav-link']) href="{{ @$link['url'] }}">{{ @$link['label'] }}</a>
     </li>
     @endforeach
 </ul>
