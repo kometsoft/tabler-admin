5140c58f79d5fb08c3b63b3e519f75fba55207aa
aerrata <0x0976@gmail.com>
2022-10-04 16:09:04 +0800

    feat: add blank page

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,16 @@
+@extends('tabler::layouts.stacked.index')
+
+@section('header')
+<x-tabler::page-header title="Untitled">
+    <div class="btn-list">
+        <x-tabler::button href="#" class="btn btn-primary" icon="plus" label="Create"></x-tabler::button>
+    </div>
+</x-page-header>
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+    </div>
+</div>
+@endsection
