ac61ca7588cadc50ec1121a99631102fda5bb128
aerrata <0x0976@gmail.com>
2022-11-02 08:45:42 +0800

    feat

--- AppServiceProvider.php
+++ AppServiceProvider.php
@@ -27,28 +27,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . 'routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 
-        $this->loadViewsFrom(__DIR__ . 'resources/views', 'tabler');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'tabler');
 
         $this->publishes([
-            __DIR__ . 'config/tabler.php' => config_path('tabler.php'),
+            __DIR__ . '/config/tabler.php' => config_path('tabler.php'),
         ], 'tabler-config');
 
         $this->publishes([
-            __DIR__ . 'app/Models/stubs' => app_path('Models'),
+            __DIR__ . '/app/Models/stubs' => app_path('Models'),
         ], 'tabler-models');
 
         $this->publishes([
-            __DIR__ . 'stubs' => base_path('stubs'),
+            __DIR__ . '/stubs' => base_path('stubs'),
         ], 'tabler-stubs');
 
         $this->publishes([
-            __DIR__ . 'database/migrations' => database_path('migrations')
+            __DIR__ . '/database/migrations' => database_path('migrations')
         ], 'tabler-migrations');
 
         $this->publishes([
-            __DIR__ . 'public/vendor' => public_path('vendor'),
+            __DIR__ . '/public/vendor' => public_path('vendor'),
         ], 'tabler-asset');
     }
 }
